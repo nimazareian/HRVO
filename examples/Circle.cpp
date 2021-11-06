e87a98eaacc306305c81246191b084b62c7ab536
Nima Zareian <nimazareian80@gmail.com>
2021-11-06 13:51:09 -0700

    Added velocity and speed to csv output from the HRVO simulator

--- examples/Circle.cpp
+++ examples/Circle.cpp
@@ -51,7 +51,7 @@
 using namespace hrvo;
 
 const float HRVO_TWO_PI = 6.283185307179586f;
-const int NUM_ROBOTS = 25;
+const int NUM_ROBOTS = 5;
 const float ROBOT_RADIUS = 0.09f;
 const float RADIUS_SCALE = 2.0f;
 
@@ -62,42 +62,44 @@ int main()
 	simulator.setTimeStep(1.f/30);
 	simulator.setAgentDefaults(1.f, 10, ROBOT_RADIUS * RADIUS_SCALE, ROBOT_RADIUS * RADIUS_SCALE, 2.0f, 2.0f);
 
-    float robot_starting_angle_dif = HRVO_TWO_PI / NUM_ROBOTS;
-    float circle_radius = float(NUM_ROBOTS) / 10;
-    
-    simulator.addAgent(Vector2(0.f, 0.f), simulator.addGoal(Vector2(0.f, 0.f)));
+    /** Add robots around circle with one in the center **/
+    // float robot_starting_angle_dif = HRVO_TWO_PI / NUM_ROBOTS;
+    // float circle_radius = std::max(float(NUM_ROBOTS) / 10, 2.f);
+    // simulator.addAgent(Vector2(0.f, 0.f), simulator.addGoal(Vector2(0.f, 0.f)));
+    // for (std::size_t i = 0; i < NUM_ROBOTS; ++i) {
+	// 	const Vector2 position = circle_radius * Vector2(std::cos(i * robot_starting_angle_dif), std::sin(i * robot_starting_angle_dif));
+	// 	simulator.addAgent(position, simulator.addGoal(-position));
+	// }
+
+    /** Add robots in a vertical line where they all have to move down **/
+    const Vector2 goal_offset = Vector2(0.f, -4.f);
     for (std::size_t i = 0; i < NUM_ROBOTS; ++i) {
-		const Vector2 position = circle_radius * Vector2(std::cos(i * robot_starting_angle_dif), std::sin(i * robot_starting_angle_dif));
-		simulator.addAgent(position, simulator.addGoal(-position));
+		const Vector2 position = Vector2(0.f, static_cast<float>(i) / 5);
+		simulator.addAgent(position, simulator.addGoal(position + goal_offset));
 	}
 
-    // Column name
-    // for (int robot_id = 0; robot_id < NUM_ROBOTS + 1; ++robot_id) {
-    //     std::cout << "robot " << robot_id;
-    //     if (robot_id != NUM_ROBOTS - 1)
-    //     {
-    //         std::cout << ",";
-    //     }
-    // }
-    // std::cout << std::endl;
-    std::cout << "frame,robot_id,x,y,has_collided" << std::endl;
+    // Column Names
+    // TODO: Can add velocity_x,velocity_y,speed columns
+    std::cout << "frame,time,robot_id,x,y,velocity_x,velocity_y,speed,has_collided" << std::endl;
 
+    const unsigned int num_robots = simulator.getNumAgents();
+    std::vector<float> prev_x_pos_arr(num_robots);
+    std::vector<float> prev_y_pos_arr(num_robots);
+    float prev_frame_time = 0.f;
     unsigned int frame = 0;
 	do {
-//#if HRVO_OUTPUT_TIME_AND_POSITIONS
-//		std::cout << simulator.getGlobalTime();
-        unsigned int num_robots = simulator.getNumAgents();
+		float time = simulator.getGlobalTime();
         for (unsigned int robot_id = 0; robot_id < num_robots; robot_id++)
         {
             Vector2 curr_robot_pos = simulator.getAgentPosition(robot_id);
-            float curr_robot_rad = ROBOT_RADIUS; // simulator.getAgentRadius(robot_id);
+            float curr_robot_rad = ROBOT_RADIUS;
 
             // Check for collision with other robots
             bool has_collided = false;
             for (unsigned int other_robot_id = 0; other_robot_id < num_robots; other_robot_id++)
             {
                 Vector2 other_robot_pos = simulator.getAgentPosition(other_robot_id);
-                float other_robot_rad = ROBOT_RADIUS; // simulator.getAgentRadius(other_robot_id);
+                float other_robot_rad = ROBOT_RADIUS; 
                 if (robot_id != other_robot_id)
                 {
                     if (absSq(curr_robot_pos - other_robot_pos) < std::pow(curr_robot_rad + other_robot_rad, 2.f))
@@ -109,35 +111,33 @@ int main()
                 
             }
             
-            std::cout << frame << "," << robot_id << "," << curr_robot_pos.getX() << "," << curr_robot_pos.getY() << "," << int(has_collided) << std::endl;
+            // Velocity and Speed measured in m/s
+            float velocity_x = 0.f;
+            float velocity_y = 0.f;
+            float speed = 0.f;
+            float delta_time = time - prev_frame_time;
+            // Get current robots velocity
+            if (frame != 0 && delta_time != 0)
+            {
+                float prev_x_pos = prev_x_pos_arr[robot_id];
+                float prev_y_pos = prev_y_pos_arr[robot_id];
+
+                float curr_x_pos = curr_robot_pos.getX();
+                float curr_y_pos = curr_robot_pos.getY();
+
+                velocity_x = (curr_x_pos - prev_x_pos) / delta_time;
+                velocity_y = (curr_y_pos - prev_y_pos) / delta_time;
+                speed = std::pow(std::pow(velocity_x, 2.f) + std::pow(velocity_y, 2.f), 0.5f);
+
+                // update previous robot x and y position  
+                prev_x_pos_arr[robot_id] = curr_x_pos;
+                prev_y_pos_arr[robot_id] = curr_y_pos;
+            }
+            
+            std::cout << frame << "," << time << "," << robot_id << "," << curr_robot_pos.getX() << "," << curr_robot_pos.getY() << "," << velocity_x << "," << velocity_y << "," << speed << "," << int(has_collided) << std::endl;
         }
         frame++;
-        
-
-
-        // for (int j = 0; j < 2; j++)
-        // {
-        //     for (std::size_t i = 0; i < simulator.getNumAgents(); ++i)
-        //     {
-        //         if (j == 0)
-        //         {
-        //             std::cout << curr_robot_pos.getX();
-        //         }
-        //         else
-        //         {
-        //             std::cout << curr_robot_pos.getY();
-        //         }
-
-        //         if (i != simulator.getNumAgents() - 1)
-        //         {
-        //             std::cout << ",";
-        //         }
-        //     }
-        //     std::cout << std::endl;
-        // }
-
-//#endif /* HRVO_OUTPUT_TIME_AND_POSITIONS */
-
+        prev_frame_time = time;
 		simulator.doStep();
 	}
 	while (!simulator.haveReachedGoals());
